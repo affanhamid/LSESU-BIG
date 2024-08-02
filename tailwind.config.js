1440f5e199d8c5f98de82dec9a1d27ca71ed7c22
affanhamid <affanhamid007@gmail.com>
2024-08-02 13:27:54 +0530

    trying something

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/components/*.tsx", "./src/app/**/*.tsx"],
+  theme: {
+    extend: {
+      colors: {
+        fontColor: "#EEEEEE",
+        black: "#0E0E10",
+        background: "#0C1A3B",
+      },
+      fontFamily: {
+        textFont: ["Playfair Display", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
