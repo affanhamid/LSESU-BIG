943d2664535d8cb58301ebaae25a7fc0ed4d7886
affanhamid <affanhamid007@gmail.com>
2024-08-07 19:27:21 +0530

    added temp pages for resources and tracker

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -7,7 +7,12 @@ const Resources = () => {
     <main>
       <Navbar />
       <ResourcesDashboard />
-      <section className="pt-80 bg-white"></section>
+      <section className="pt-72 bg-white">
+        <h1 className="text-3xl text-center pb-20">
+          This part of the website is currently under development. In the
+          future, it will feature all the free resources of BIG{" "}
+        </h1>
+      </section>
     </main>
   );
 };
--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import Navbar from "@/Components/Navbar";
 
 const Tracker = () => {
-    return (
-        <div>Tracker</div>
-    )
-}
+  return (
+    <main>
+      <Navbar />
+      <section className="pt-72 bg-white">
+        <h1 className="text-3xl text-center pb-20 px-20">
+          This part of the website is currently under development. In the future
+          it will have an automated tracker of openings and closings of all
+          major applications
+        </h1>
+      </section>
+    </main>
+  );
+};
 
-export default Tracker
+export default Tracker;
