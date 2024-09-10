92db7f12f028a28985257442bf27d92c16b17667
affanhamid <affanhamid007@gmail.com>
2024-09-10 16:21:15 +0530

    Add speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Footer, Navbar } from "@/Components";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "LSESU Business and Investment Group",
@@ -24,6 +25,7 @@ export default function RootLayout({
         <Footer />
       </body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
