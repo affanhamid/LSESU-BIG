eaf10dee52ad270cc767717197c87cea5e33e9d8
Varmisan Thivagharen <varmisan@legafy.co.uk>
2024-09-15 14:17:59 +0100

    pubhtml for sheet

--- src/app/tracker/Sheet.tsx
+++ src/app/tracker/Sheet.tsx
@@ -44,7 +44,7 @@ const Sheet = () => {
       <section className="p-5 bg-white">
         <iframe
           className="w-full h-[80vh] outline-none"
-          src="https://docs.google.com/spreadsheets/d/1Zk13bgREvD2Fev2XKJasyyx-BV2SYimA/preview?gid=1922076053#gid=1922076053"
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ-hdqM02siTVA2J0HyEzYicNGw2Xw96LcYmseEp5QaDsaZ0Rc1g8RpkvnDqMW8Ew/pubhtml?widget=true&amp;headers=false"
         ></iframe>
       </section>
     );
