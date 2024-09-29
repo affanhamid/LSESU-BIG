2ec52db56b1ba8c8a1fd045c1f1ead16e7d721bc
Varmisan Thivagharen <varmisan@legafy.co.uk>
2024-09-29 20:30:32 +0100

    updated resources

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -12,29 +12,31 @@ const Resources = () => {
         <Image
           src={resourcesImage}
           className="w-full h-[75vh] object-cover z-0 brightness-50"
-          alt=""
+          alt="Resources Background"
         />
         <h1 className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-4xl lg:text-5xl font-bold text-center my-10 z-60 text-white">
           RESOURCES
         </h1>
       </header>
       <section className="py-16 bg-white flex flex-col items-center justify-center">
-        <p className="text-2xl mb-10 px-96 text-center">
+        <p className="text-2xl mb-10 px-4 sm:px-96 text-center">
           Find our industry-leading resource packs below ( More to come soon! )
         </p>
-        <div className="grid grid-cols-3">
-          <Resource
-            link={
-              "https://utfs.io/f/2JsPsN0kG4dUOi59vnh5ZHjPUWw0N97KzyleQfmGRcCFEvXr"
-            }
-            thumbnail="https://utfs.io/f/2JsPsN0kG4dUNk99Rjoy9s7WdzcfkhqYZ6iSbgAPtL1FlMUa"
-          />
-          <Resource
-            link={
-              "https://drive.google.com/file/d/1JW-xAENoNT0THhmuP-SyW4CLAIi--is0/view"
-            }
-            thumbnail="https://utfs.io/f/2JsPsN0kG4dUE6WK3wR2ENeIDxX4bQRMCta3qGyfKuPJj09g"
-          />
+        <div className="w-full overflow-x-scroll sm:overflow-auto">
+          <div className="flex sm:grid sm:grid-cols-3 gap-4 sm:gap-0 whitespace-nowrap sm:whitespace-normal">
+            <Resource
+              link={
+                "https://utfs.io/f/2JsPsN0kG4dUOi59vnh5ZHjPUWw0N97KzyleQfmGRcCFEvXr"
+              }
+              thumbnail="https://utfs.io/f/2JsPsN0kG4dUNk99Rjoy9s7WdzcfkhqYZ6iSbgAPtL1FlMUa"
+            />
+            <Resource
+              link={
+                "https://drive.google.com/file/d/1JW-xAENoNT0THhmuP-SyW4CLAIi--is0/view"
+              }
+              thumbnail="https://utfs.io/f/2JsPsN0kG4dUE6WK3wR2ENeIDxX4bQRMCta3qGyfKuPJj09g"
+            />
+          </div>
         </div>
       </section>
       <Footer />
